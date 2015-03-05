c2ea6984c575d3f0427d41f2b52731e3915f8e4a
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-05 15:38:36 +0100

    Document usage

--- README.md
+++ README.md
@@ -9,9 +9,90 @@ Get amendment data from the French National Assembly in Meteor
 
 	meteor add sgmap:assemblee-nationale
 
-The, on the server, you get access to a global `AssembleeNationale` namespace, from which you can call `AssembleeNationale.getAmendements(textId, [organismId], [legislature])`.
+Then, on the server, you get access to a global `AssembleeNationale` namespace, which gives you the ability to asynchronously retrieve an array of amendments descriptions based on a law project identifier with the following function call:
 
-These parameters are described in the `server/assembleeNationale.js` file.
+```javascript
+AssembleeNationale.getAmendements(textId, organismId, legislature, callback);
+```
+
+Your callback will be called with two parameters: an error or null if none occurred, and an array containing [amendment descriptions](#amendments-descriptions), sorted by examination order.
+
+
+### Identifying a project law
+
+A law project is identified by the combination of three codes.
+
+
+#### textId
+
+The identifier for the text itself (_“numéro du texte”_). This identifier is coded on four characters that are arbitrarily attributed by the Assembly, and an optional fifth according to the following rules (in French):
+
+- `''` : texte hors Projet de Loi de Finances, première délibération
+- `'S'` : texte hors Projet de Loi de Finances, seconde délibération
+- `'A'` : Projet de Loi de Finances, première partie, première délibération
+- `'B'` : Projet de Loi de Finances, première partie, seconde délibération
+- `'C'` : Projet de Loi de Finances, deuxième partie, première délibération
+- `'D'` : Projet de Loi de Finances, deuxième partie, seconde délibération
+
+**Example**: the first Macron law project text identifier is `2447`.
+
+
+#### organismId
+
+The identifier of the legislative organism in charge of examining the law project (_“abréviation tribun de l’organe examinant”_). This identifier is usually one of the following (in French), but the Assembly may name special commissions, and we can't predict these names…
+
+- organe de type Assemblée Nationale : 'AN'
+- organe de type commission permanente législative :
+	- Défense : `CION_DEF`
+	- Affaires étrangères : `CION_AFETR`
+	- Finances : `CION_FIN`
+	- Lois : `CION_LOIS`
+	- Affaires culturelles et éducation : `CION-CEDU`
+	- Affaires économiques : `CION-ECO`
+	- Développement durable : `CION-DVP`
+	- Affaires sociales : `CION-SOC`
+- organe de type commission spéciale : `CION_SPE`
+
+**Example**: the first Macron law project organism identifier is `CSCRACTIV`.
+
+
+#### legislature
+
+The identifier of the legislature that will examine the law project. This number was [14](https://fr.wikipedia.org/wiki/XIVe_législature_de_la_Cinquième_République_française) in 2012 and is incremented by 1 each time a new Assembly is elected. Under French law at time of writing, this is every 5 years, unless it is dissolved.
+
+**Example**: the first Macron law project legislature identifier is `14`.
+
+
+### Amendments descriptions
+
+The resulting amendments descriptions are basically a dump of what the National Assembly sends (typos included), with a few fields normalised. Please add your own [normalisers](https://github.com/sgmap/meteor-assemblee-nationale/blob/master/server/normalizers.js) if you feel like it!
+
+Here is an example amendment description:
+
+```json
+{
+	"alineaLabel": "av 1",
+	"auteurGroupe": "UDI",
+	"auteurLabel": "M. PANCHER",
+	"auteurLabelFull": "M. PANCHER Bertrand",
+	"discussionCommune": "",
+	"discussionCommuneAmdtPositon": "",
+	"discussionCommuneSsAmdtPositon": "",
+	"discussionIdentique": "",
+	"discussionIdentiqueAmdtPositon": "",
+	"discussionIdentiqueSsAmdtPositon": "",
+	"missionLabel": "",
+	"numero": "SPE862",
+	"parentNumero": "",
+	"place": {
+		"article": 1,
+		"raw": "Article PREMIER",
+		"type": "article",
+	},
+	"position": 1,
+	"sort": "Adopté"
+}
+```
 
 
 ## Licence
--- server/assembleeNationale.js
+++ server/assembleeNationale.js
@@ -17,42 +17,53 @@ AssembleeNationale = {
 	AFTER	: 1,	// Symbol, do NOT rely on this value
 
 
-	/**
-	*@param	{Number}	legislature	Numéro de la législature.
-	*@param	{String}	textId	Numéro du texte codé sur 4 caractères + suffixe :
-	*	- ''  : texte hors PLF, première délibération
-	*	- 'S' : texte hors PLF, seconde délibération
-	*	- 'A' : PLF, première partie, première délibération
-	*	- 'B' : PLF, première partie, seconde délibération
-	*	- 'C' : PLF, deuxième partie, première délibération
-	*	- 'D' : PLF, deuxième partie, seconde délibération
-	*@param	{String}	organismId	Abréviation tribun de l’organe examinant :
-	*	- organe de type Assemblée nationale : AN
-	*	- organe de type commission permanente législative :
-	*		- Défense	'CION_DEF'
-	*		- Affaires étrangères	'CION_AFETR'
-	*		- Finances	'CION_FIN'
-	*		- Lois	'CION_LOIS'
-	*		- Affaires culturelles et éducation	'CION-CEDU'
-	*		- Affaires économiques	'CION-ECO'
-	*		- Développement durable	 'CION-DVP'
-	*		- Affaires sociales	'CION-SOC'
-	*	- organe de type commission spéciale :	'CION_SPE'
+	/** Retrieve and normalize all amendments from the given law project.
+	*
+	*@param	{String}	textId	Project law number.
+	*@param	{String}	organismId	Identifier of the legislative organism in charge of examining the law project.
+	*@param	{Number}	legislature	Legislature number.
+	*@param	{Function<Error|null,Array<Object>>}	A callback that will be passed the normalized amendments as a second argument or, if an error occurred, this error as a first argument.
+	*/
+	getAmendements: function getAmendements(textId, organismId, legislature, callback) {
+		AssembleeNationale.getAmendementsJSON(textId, organismId, legislature, function(error, result) {
+			if (error)
+				return callback(error);
+
+			callback(null, AssembleeNationale.normalizeAmendements(result));
+		});
+	},
+
+	/** Construct the URL to the XML amendments list based on the given law project identifiers.
+	*
+	*@param	{String}	textId	Project law number.
+	*@param	{String}	[organismId = 'AN']	Identifier of the legislative organism in charge of examining the law project.
+	*@param	{Number}	[legislature = 14]	Legislature number.
+	*@returns	{String}	The URL to the XML file listing all amendments for the given law project.
 	*/
 	getAmendementsURL: function getAmendementsURL(textId, organismId, legislature) {
 		if (! textId)
 			throw new ReferenceError('You need to specify a text number.');
 
 		organismId = organismId || 'AN';
-		legislature = legislature || 14;	// unless unexpected surprise, will be 14 from 2012 to 2017
+		legislature = legislature || 14;	// unless the Assembly is dissolved, will be 14 from 2012 to 2017
 
 		return [ 'http://www.assemblee-nationale.fr', legislature, 'amendements', textId, organismId, 'liste.xml' ].join('/');
 	},
 
+	/** Retrieve the XML file listing all amendments for the given law project.
+	*
+	*@see	getAmendementsURL
+	*@private
+	*/
 	getAmendementsXML: function getAmendementsXML(textId, organismId, legislature, callback) {
 		HTTP.get(AssembleeNationale.getAmendementsURL(textId, organismId, legislature), { timeout: 20 * 1000 }, callback);
 	},
 
+	/** Retrieve the XML file listing all amendments for the given law project and convert it to JSON.
+	*
+	*@see	getAmendementsURL
+	*@private
+	*/
 	getAmendementsJSON: function getAmendementsJSON(textId, organismId, legislature, callback) {
 		AssembleeNationale.getAmendementsXML(textId, organismId, legislature, function(error, result) {
 			if (error)
@@ -62,6 +73,13 @@ AssembleeNationale = {
 		});
 	},
 
+	/** Normalize raw amendments file to an array of amendments descriptions, sorted by order of examination.
+	*
+	*@param	{Array<Object<String,String>>}
+	*@returns	{Array<Object>}	A list of all passed amendments, normalized by calling all functions in Normalizers on the matching property.
+	*@see	Normalizers
+	*@private
+	*/
 	normalizeAmendements: function normalizeAmendements(data) {
 		return data.amdtsParOrdreDeDiscussion.amendements[0].amendement.map(function(amendementWrapper) {
 			var result = amendementWrapper['$'];
@@ -70,15 +88,6 @@ AssembleeNationale = {
 				result[propertyToNormalize] = Normalizers[propertyToNormalize](result[propertyToNormalize]);
 
 			return result;
-		});
-	},
-
-	getAmendements: function getAmendements(textId, organismId, legislature, callback) {
-		AssembleeNationale.getAmendementsJSON(textId, organismId, legislature, function(error, result) {
-			if (error)
-				return callback(error);
-
-			callback(null, AssembleeNationale.normalizeAmendements(result));
 		});
 	}
 }
